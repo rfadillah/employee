b4d60ff68198985faea3b43b0f5713624c79a8c1
Rizki fadillah <rizki.fadillah.20@student.is.ittelkom-sby.ac.id>
2023-05-10 11:27:39 +0700

    first commit

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+
+class EmployeeController extends Controller
+{
+    function index()
+    {
+        $pageTitle = 'Employee List';
+        return view('Employee.index', ['pageTitle' => $pageTitle]);
+    }
+
+    public function create()
+    {
+        $pageTitle = 'Create Employee';
+
+        return view('employee.create', compact('pageTitle'));
+    }
+
+    public function store(Request $request)
+    {
+        $messages = [
+            'required' => ':Attribute harus diisi.',
+            'email' => 'Isi :attribute dengan format yang benar',
+            'numeric' => 'Isi :attribute dengan angka'
+        ];
+        $validator = Validator::make($request->all(), [
+            'firstName' => 'required',
+            'lastName' => 'required',
+            'email' => 'required|email',
+            'age' => 'required|numeric',
+        ], $messages);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+        return $request->all();
+    }
+}
